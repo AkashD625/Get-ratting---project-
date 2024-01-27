fe18667eb0e2b615c05eb7c02d588f3b2c460dc8
Akash <divateakash21@gmail.com>
2024-01-28 00:10:47 +0530

    #

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function onclick1(){
     console.log("Clicked");
     var ob=document.getElementById("ob").value;
     var tot=document.getElementById("to").value;
+    if(ob==null||tot==null){
+        document.getElementById("final_per").innerText='Please enter your obtained marks and total marks';
+
+    }
     var result=(ob/tot)*100;
     document.getElementById("final_per").innerText=`Congratulations your percentage is ${result}%`;
 }
