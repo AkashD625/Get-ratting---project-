2298744b1d9051375b9d9177be75ede6c17e6342
Akash <divateakash21@gmail.com>
2024-02-05 16:08:35 +0530

    added alert

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function onclick1(){
     console.log("Clicked");
     var ob=document.getElementById("ob").value;
     var tot=document.getElementById("to").value;
-    if(ob==null||tot==null){
-        document.getElementById("final_per").innerText='Please enter your obtained marks and total marks';
-
+    if(ob>tot){
+        alert("Obtained marks should be smaller then or equal to total marks");
+        return;
     }
     var result=(ob/tot)*100;
     document.getElementById("final_per").innerText=`Congratulations your percentage is ${result}%`;
